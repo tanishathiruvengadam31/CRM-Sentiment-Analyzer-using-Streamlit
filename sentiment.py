a83385a269e937d211c4a44ffd2fbe2429353ecc
Preethi-2772 <lpreethi2772@gmail.com>
2026-03-23 21:06:39 +0530

    Fix NLTK for Streamlit

--- sentiment.py
+++ sentiment.py
@@ -1,27 +1,21 @@
-from textblob import TextBlob
-import re
-from collections import defaultdict
-
-import nltk
 import os
+import nltk
 
-# Streamlit-safe path
+# Force a writable directory (critical for Streamlit)
 nltk_data_path = "/tmp/nltk_data"
 os.makedirs(nltk_data_path, exist_ok=True)
 
 # Tell nltk to use this path
-nltk.data.path.append(nltk_data_path)
+nltk.data.path = [nltk_data_path]
 
-# Download required corpora
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt', download_dir=nltk_data_path)
+# Download required resources
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
 
-try:
-    nltk.data.find('taggers/averaged_perceptron_tagger')
-except LookupError:
-    nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+from textblob import TextBlob
+import re
+from collections import defaultdict
+
 # -------------------------------
 # STEP 1: CLEAN TEXT
 # -------------------------------
